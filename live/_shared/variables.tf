bf53fbfe7be8bf9f8facb20964fab69595b5a3ef
Nghia-VanTrong <nghiavt@qnsc.vn>
2026-06-27 20:02:18 +0700

    fix: correct github_org default to QNSC-VN

--- live/_shared/variables.tf
+++ live/_shared/variables.tf
@@ -1,5 +1,5 @@
 variable "github_org" {
   type        = string
   description = "GitHub organisation or username that owns rally-api, rally-web, rally-infra repos"
-  default     = "nghiavt1802"
+  default     = "QNSC-VN"
 }
